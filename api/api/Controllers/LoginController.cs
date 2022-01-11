d60d426ac8983691e99dea9cfe116c7a0f4db2ab
Matheus Uchoas Veiga <matheuzuchoas@gmail.com>
2022-01-10 23:29:09 -0300

    Atualização do modelo da API. Agora esta chamavel.

---
+++ api/api/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [Route("api/login")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        [Route("/signup")]
+        public void SignUp(string data)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("signin")]
+        public IActionResult SignIn()
+        {
+            Result result = Result.GetInstance;
+            List<string> list = new List<string>(){
+                "Teste 1",
+                "Teste 2"
+            };
+
+            result = Result.CreateResult(false, "Usuários listados com sucesso!", list);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/forgotpassword")]
+        public ActionResult ResetPassword(string username)
+        {
+            try
+            {
+                string msg = "Password Updated!";
+                return Ok(msg);
+            }
+            catch (System.Exception error)
+            {
+                throw error;
+            }
+        }
+    }
+}
